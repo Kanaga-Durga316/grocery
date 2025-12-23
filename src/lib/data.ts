5b4baa24e7d56ec53156ba15f22425361eeed312
Kanaga durga M <murugesan99659100@gmail.com>
2025-12-23 06:53:03 +0000

    In personal care & wellness category ,there is a sub category named as h

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1451,7 +1451,7 @@ const products: Product[] = [
         price: 99,
         stock: 180,
         categoryId: 'personal-care-wellness',
-        imageUrl: '/PersonalCare/prod-handwash.png',
+        imageUrl: '/PersonalCare/dettol handwash refill.png',
         imageHint: 'handwash refill',
         sellerId: 'seller-1',
         subCategory: 'Bath & Body',
@@ -1611,6 +1611,7 @@ export const getReviewsForProduct = (productId: string) => reviews.filter(r => r
 export const getOrders = () => orders;
 export const getOrderById = (id: string) => orders.find(o => o.id === id);
 export const getOrdersByUser = (userId: string) => orders.filter(o => o.userId === userId);
+
 
 
 
