2557993f9a75ed07db293bab29c4b087317592a7
Kanaga durga M <murugesan99659100@gmail.com>
2025-12-23 08:04:06 +0000

    In personal care & wellness category ,there is a sub category named as i

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1490,7 +1490,7 @@ const products: Product[] = [
         price: 550,
         stock: 80,
         categoryId: 'personal-care-wellness',
-        imageUrl: '/PersonalCare/prod-diapers.png',
+        imageUrl: '/PersonalCare/pampers baby diapers.png',
         imageHint: 'baby diapers',
         sellerId: 'seller-1',
         subCategory: 'Infant Hygiene',
@@ -1611,6 +1611,7 @@ export const getReviewsForProduct = (productId: string) => reviews.filter(r => r
 export const getOrders = () => orders;
 export const getOrderById = (id: string) => orders.find(o => o.id === id);
 export const getOrdersByUser = (userId: string) => orders.filter(o => o.userId === userId);
+
 
 
 
